86ce0bfc385cd3ad6de43a15dde6b4257b00b5ec
thiago <thiagovporfirio@gmail.com>
2023-12-22 16:53:57 -0300

    add function of modification item

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const axios = require("axios");
 
-let access_token = "838yPlWznA7JLHi0GWUbx0NYWIl0VrlR";
-const refresh_token = "rPCrdCLA2A7YfrJQSLGiYsjeDsSV0hQO";
+let access_token = "YYAE2Lr7YvdWe36zAzz7IwnR7097vt87";
+const refresh_token = "q3YJHscbwCKoD3O1rIcMnkwMtqkuy2hX";
 
 const client_id = "2nkynyo1lXuHUN9pLZqiqvGS0XTONlCn";
 const client_secret = "a8bjnGVvDnM0RK8UJIEHKsg7n3A9AbHp";
+
+let ultimoPedidoArmazenado = null;
 
 async function obterNovoAccessToken() {
   try {
@@ -19,7 +21,9 @@ async function obterNovoAccessToken() {
     );
 
     access_token = response.data.access_token;
+    refresh_token = response.data.refresh_token; // Atualiza o refresh_token
     console.log("Novo Access Token obtido:", access_token);
+    console.log("Novo Refresh Token obtido:", refresh_token);
   } catch (error) {
     console.error("Erro ao obter novo Access Token:", error.message);
   }
@@ -32,35 +36,38 @@ async function fazerRequisicoes() {
       await obterNovoAccessToken();
     }
 
+    const dataDeHoje = new Date().toISOString().split("T")[0];
     // Fazer a primeira requisição para o servidor inicial
-    const respostaServidor1 = await axios.get("https://api.tagplus.com.br/pedidos?status=B", {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-        "X-Api-Version": "2.0",
-        "Content-Type": "application/json",
-      },
-    });
-
+    const respostaServidor1 = await axios.get(
+      `https://api.tagplus.com.br/pedidos?status=B&data_criacao=${dataDeHoje}`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "X-Api-Version": "2.0",
+          "Content-Type": "application/json",
+        },
+      }
+    );
     // Extrair os dados da resposta, se necessário
-    const dadosResposta1 = respostaServidor1.data;
+    const pedidos = respostaServidor1.data;
+    if (pedidos.length > 0) {
+      // Obter o último item da lista
+      const ultimoPedidoAtual = pedidos[pedidos.length - 1];
+      // Comparar com o último armazenado
+      if (
+        !ultimoPedidoArmazenado ||
+        ultimoPedidoAtual.id !== ultimoPedidoArmazenado.id
+      ) {
+        console.log(
+          "Houve uma alteração no último pedido. Atualizando variável."
+        );
+        ultimoPedidoArmazenado = ultimoPedidoAtual;
+        // await gerarPedidoEntrega(ultimoPedidoAtual);
+      } else {
+        console.log("Retorna o ultimo item, se for o mesmo: ", ultimoPedidoAtual)
+      }
+    }
 
-    // // Fazer a segunda requisição para o segundo servidor com os dados da primeira resposta
-    // const respostaServidor2 = await axios.post(
-    //   "URL_SERVIDOR_2",
-    //   { dados: dadosResposta1 },
-    //   {
-    //     headers: {
-    //       Authorization: `Bearer ${access_token}`,
-    //       "X-Api-Version": "2.0",
-    //       "Content-Type": "application/json",
-    //     },
-    //   }
-    // );
-
-    // Processar a resposta final, se necessário
-    // const resultadoFinal = respostaServidor2.data;
-
-    console.log("Resultado Final:", dadosResposta1);
   } catch (erro) {
     console.error("Ocorreu um erro:", erro);
 
@@ -71,4 +78,30 @@ async function fazerRequisicoes() {
   }
 }
 
-// setInterval(fazerRequisicoes, 10 * 1000);
+// async function gerarPedidoEntrega(ultimoPedido) {
+//   // Lógica para fazer a requisição para gerar um pedido de entrega no outro servidor
+//   try {
+//     const respostaPedidoEntrega = await axios.post(
+//       "URL_SERVIDOR_ENTREGA",
+//       {
+//         dados: ultimoPedido,
+//       },
+//       {
+//         headers: {
+//           Authorization: `Bearer ${access_token}`,
+//           "X-Api-Version": "2.0",
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
+
+//     console.log(
+//       "Pedido de entrega gerado com sucesso:",
+//       respostaPedidoEntrega.data
+//     );
+//   } catch (error) {
+//     console.error("Erro ao gerar pedido de entrega:", error.message);
+//   }
+// }
+
+setInterval(fazerRequisicoes, 10 * 1000);
